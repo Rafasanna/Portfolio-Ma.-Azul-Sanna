6be5647bb01d27a6f14ef1ff40bc5b1103d7b7d2
Rafasanna <rafaelasanna1511@gmail.com>
2026-05-03 00:41:46 -0300

    feat: merge artrosis with tendinosas in rehab items

--- src/app/page.js
+++ src/app/page.js
@@ -15,8 +15,7 @@ export default function Home() {
 
   const rehabItems = [
     "Fractura de dedos, mano, muñeca y antebrazo.",
-    "Lesiones tendinosas (aparato flexor y extensor), Tenosinovitis Estenosante.",
-    "Artrosis y Artritis.",
+    "Lesiones tendinosas (aparato flexor y extensor), Tenosinovitis Estenosante. Artrosis y Artritis.",
     "Deformidades articulares (Cuello de Cisne, Boutonniere, Mallet Finger).",
     "Tratamiento de cicatrices.",
     "Confección de férulas."
